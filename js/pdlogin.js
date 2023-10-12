2ca9e6c8f20d8a1af5247633bb0f560637734787
zyh <1428689567@qq.com>
2023-10-12 09:23:16 +0800

    链接跳转

--- js/pdlogin.js
+++ js/pdlogin.js
@@ -1,7 +1,6 @@
 var a = document.getElementById('zb');
 var login = JSON.parse(localStorage.getItem('login'));
 var tel = JSON.parse(localStorage.getItem('tel'));
-console.log(a);
 a.onclick = function () {
     if (login) {
         location.href = ("./my2.html")
